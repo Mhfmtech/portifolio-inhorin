8787f1aa950cf99c048e6d194ec083175b018136
MauroHenrique22 <90928330+MauroHenrique22@users.noreply.github.com>
2025-06-06 16:16:13 -0300

    Revert "tags"
    This reverts commit 7bfd5d4faf15c99cecad330c1f9abe7bb746c5e1.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,8 +22,5 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
       ]
     }
-  },
-  plugins: [
-    '~/plugins/analytics.client.ts'
-  ]
+  }
 })
